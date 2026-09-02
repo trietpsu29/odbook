92a9b0c785035ad1e9aef78c489d95a2eb4d17dc
Triet Psu <112594221+trietpsu29@users.noreply.github.com>
2026-09-03 01:26:32 +0700

    Add header styles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!
+  before_action :redirect_authenticated_user
   def home
+  end
+
+private
+  def redirect_authenticated_user
+    redirect_to posts_path if user_signed_in?
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [ :edit, :update, :destroy ]
   before_action :authorize_post, only: [ :edit, :update, :destroy ]
+  layout "authenticated"
 
   def index
     @posts = Post.all
