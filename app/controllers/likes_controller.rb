f70a5717ff6f12b5b683abe88d88237a27ae1485
Triet Psu <112594221+trietpsu29@users.noreply.github.com>
2026-09-03 12:44:03 +0700

    Add Turbo Stream like updates

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,16 +3,23 @@ class LikesController < ApplicationController
     @post = Post.find(params[:post_id])
     @like = @post.likes.build(user: current_user)
     if @like.save
-      redirect_to posts_path
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to posts_path }
+      end
     else
       redirect_to posts_path, alert: "Something went wrong"
     end
   end
 
   def destroy
-    @like = current_user.likes.find(params[:id])
+    @post = Post.find(params[:post_id])
+    @like = @post.likes.find(params[:id])
     if @like.destroy
-      redirect_to posts_path
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to posts_path }
+      end
     else
       redirect_to posts_path, alret: "Something went wrong"
     end
