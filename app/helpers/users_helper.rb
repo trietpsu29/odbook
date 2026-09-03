d8922a188dbca2513160f35775fccc8f3c4851f6
Triet Psu <112594221+trietpsu29@users.noreply.github.com>
2026-09-03 11:00:50 +0700

    Add index layout and post header styles

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  def user_avatar(user, size: 40)
+    if user.avatar.attached?
+      image_tag user.avatar, class: "avatar", size: "#{size}x#{size}"
+    elsif user.avatar_url.present?
+      image_tag user.avatar_url, class: "avatar", size: "#{size}x#{size}"
+    else
+      image_tag "default-avatar.svg", class: "avatar", size: "#{size}x#{size}"
+    end
+  end
 end
