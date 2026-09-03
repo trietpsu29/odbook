65a0dd6fabea88d647c9fc933c37f373b81860f2
Triet Psu <112594221+trietpsu29@users.noreply.github.com>
2026-09-03 17:51:31 +0700

    Add post footer styles

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -1,9 +1,18 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["section"];
+  static targets = ["section", "icon"];
 
   toggle() {
     this.sectionTarget.classList.toggle("hidden");
+    const currentSrc = this.iconTarget.src;
+    const outlineSrc = this.iconTarget.dataset.outlineSrc;
+    const fillSrc = this.iconTarget.dataset.fillSrc;
+
+    if (currentSrc.includes("comment-fill")) {
+      this.iconTarget.src = outlineSrc;
+    } else {
+      this.iconTarget.src = fillSrc;
+    }
   }
 }
