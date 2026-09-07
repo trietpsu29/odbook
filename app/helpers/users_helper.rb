9c1480067eb2cd6f9297654f73832b96ebd484f1
Triet Psu <112594221+trietpsu29@users.noreply.github.com>
2026-09-07 14:20:11 +0700

    Improve follow buttons and user filters

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -10,7 +10,8 @@ module UsersHelper
         data: data
     end
   end
-  def follow_button(user)
+
+  def follow_button(user, filter = nil)
     if user == current_user
       content_tag :div, class: "edit-profile-container" do
         link_to "Edit profile",
@@ -18,17 +19,29 @@ module UsersHelper
           class: "follow-button edit"
       end
 
-    elsif current_user.following_relationships.exists?(followed: user)
+    elsif filter == "requests"
+      request = current_user.received_follow_requests.find_by(requester: user)
 
+      render "shared/follow_request_buttons", request: request
+
+    elsif filter == "following"
       follow = current_user.following_relationships.find_by(followed: user)
 
-      button_to "Unfollow",
+      button_to "Unfolclow",
         follow_path(follow),
         method: :delete,
         class: "follow-button unfollow"
 
-    elsif current_user.sent_follow_requests.exists?(requested: user)
+    elsif filter == "followers"
+      follow = current_user.follower_relationships.find_by(follower: user)
 
+      button_to "Remove follower",
+        follow_path(follow),
+        method: :delete,
+        class: "follow-button remove-follower",
+        data: { turbo_confirm: "Are you sure you want to remove this user?" }
+
+    elsif filter == "requested"
       request = current_user.sent_follow_requests.find_by(requested: user)
 
       button_to "Cancel request",
@@ -36,27 +49,28 @@ module UsersHelper
         method: :delete,
         class: "follow-button unrequested"
 
-    elsif current_user.received_follow_requests.exists?(requester: user)
-
-      request = current_user.received_follow_requests.find_by(requester: user)
-      render "shared/follow_request_buttons", request: request
-
-    elsif current_user.follower_relationships.exists?(follower: user)
-
-      follow = current_user.follower_relationships.find_by(follower: user)
-
-      button_to "Remove follower",
-        follow_path(follow),
-        method: :delete,
-        class: "follow-button remove-follower",
-        data: { turbo_confirm: "Are you sure you want to unfollow this user?" }
-
     else
+      if current_user.following_relationships.exists?(followed: user)
+        follow = current_user.following_relationships.find_by(followed: user)
 
-      button_to "Follow",
-        user_follow_requests_path(user),
-        method: :post,
-        class: "follow-button"
+        button_to "Unfollow",
+          follow_path(follow),
+          method: :delete,
+          class: "follow-button unfollow"
+
+      elsif current_user.sent_follow_requests.exists?(requested: user)
+        request = current_user.sent_follow_requests.find_by(requested: user)
+
+        button_to "Cancel request",
+          follow_request_path(request),
+          method: :delete,
+          class: "follow-button unrequested"
+      else
+        button_to "Follow",
+          user_follow_requests_path(user),
+          method: :post,
+          class: "follow-button"
+      end
     end
   end
 end
